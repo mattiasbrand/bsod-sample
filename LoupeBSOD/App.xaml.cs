e64d65a835ef5d9e7d4ac268f6c9329bf206c5a8
Mattias Brand <mattias.brand@tobii.com>
2013-10-21 14:28:09 +0200

    Initial commit

---
+++ LoupeBSOD/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Gibraltar.Agent;
+
+namespace LoupeBSOD
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Log.StartSession();
+            base.OnStartup(e);
+        }
+        protected override void OnActivated(EventArgs e)
+        {
+            Log.Warning("Foo2", "bar", "baz");
+            base.OnActivated(e);
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.EndSession();
+            base.OnExit(e);
+        }
+    }
+}
